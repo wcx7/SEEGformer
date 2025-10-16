3ed65809434fa821d538aa062139e71506ccf460
wcx7 <66783581+wcx7@users.noreply.github.com>
2025-10-16 14:43:28 +0800

    Add comments for clarity in ChannelBasedTransformer

--- SEEGformer/ChannelBasedTransformer_fft_PE.py
+++ SEEGformer/ChannelBasedTransformer_fft_PE.py
@@ -22,12 +22,12 @@ class ChannelBasedTransformer_fft(nn.Module):
         self.fc_position_imag = nn.Linear(position_encoding.shape[1], embed_dim)
         self.fc_position_abs = nn.Linear(position_encoding.shape[1], embed_dim)
 
-        self.transformer_blocks_real = nn.ModuleList([TransformerBlock(embed_dim, num_heads, mlp_ratio, dropout) for _ in range(num_blocks)])
-        self.transformer_blocks_imag = nn.ModuleList([TransformerBlock(embed_dim, num_heads, mlp_ratio, dropout) for _ in range(num_blocks)])
-        self.transformer_blocks_abs = nn.ModuleList([TransformerBlock(embed_dim, num_heads, mlp_ratio, dropout) for _ in range(num_blocks)])
+        self.transformer_blocks_real = nn.ModuleList([TransformerBlock(embed_dim, num_heads, mlp_ratio, dropout) for _ in range(num_blocks)])    # real part
+        self.transformer_blocks_imag = nn.ModuleList([TransformerBlock(embed_dim, num_heads, mlp_ratio, dropout) for _ in range(num_blocks)])    # imaginary part
+        self.transformer_blocks_abs = nn.ModuleList([TransformerBlock(embed_dim, num_heads, mlp_ratio, dropout) for _ in range(num_blocks)])     # amplitude part
         
 
-        self.pos_embed = nn.Parameter(torch.zeros(1, n_chans + 1, position_encoding.shape[1]), requires_grad=False)  # 位置编码
+        self.pos_embed = nn.Parameter(torch.zeros(1, n_chans + 1, position_encoding.shape[1]), requires_grad=False)  # positional encoding
         
         
         self.cls_token_real = nn.Parameter(torch.zeros(1, 1, embed_dim))
@@ -35,13 +35,13 @@ class ChannelBasedTransformer_fft(nn.Module):
         self.cls_token_abs = nn.Parameter(torch.zeros(1, 1, embed_dim))
 
         
-        self.cls_weight_real = nn.Parameter(0.3333 * torch.ones(1,), requires_grad=True)
-        self.cls_weight_imag = nn.Parameter(0.3333 * torch.ones(1,), requires_grad=True)
-        self.cls_weight_abs = nn.Parameter(0.3333 * torch.ones(1,), requires_grad=True)
+        self.cls_weight_real = nn.Parameter(0.3333 * torch.ones(1,), requires_grad=True)    # classification weight of real part
+        self.cls_weight_imag = nn.Parameter(0.3333 * torch.ones(1,), requires_grad=True)    # classification weight of imaginary part
+        self.cls_weight_abs = nn.Parameter(0.3333 * torch.ones(1,), requires_grad=True)     # classification weight of amplitude part
        
-        self.classifier_real = nn.Linear(embed_dim, num_classes)
-        self.classifier_imag = nn.Linear(embed_dim, num_classes)
-        self.classifier_abs = nn.Linear(embed_dim, num_classes)
+        self.classifier_real = nn.Linear(embed_dim, num_classes)    # classifier of real part
+        self.classifier_imag = nn.Linear(embed_dim, num_classes)    # classifier of imaginary part
+        self.classifier_abs = nn.Linear(embed_dim, num_classes)     # classifier of amplitude part
 
         self.pos_embed.data.copy_(torch.from_numpy(position_encoding).float().unsqueeze(0))
 
@@ -51,7 +51,7 @@ class ChannelBasedTransformer_fft(nn.Module):
         # real
         x_real = self.fc_real(x_real)
         self.pos_embed_real = self.fc_position_real(self.pos_embed)
-        x_real = x_real + self.pos_embed_real[:, 1:, :]     # 和位置编码相加
+        x_real = x_real + self.pos_embed_real[:, 1:, :]     # add with positional encoding
         cls_token_real = self.cls_token_real + self.pos_embed_real[:, :1, :]
         cls_tokens_real = cls_token_real.expand(x_real.shape[0], -1, -1)
         x_real = torch.cat((cls_tokens_real, x_real), dim=1)        
@@ -66,7 +66,7 @@ class ChannelBasedTransformer_fft(nn.Module):
         # imag
         x_imag = self.fc_imag(x_imag)
         self.pos_embed_imag = self.fc_position_imag(self.pos_embed)       
-        x_imag = x_imag + self.pos_embed_imag[:, 1:, :]     # 和位置编码相加
+        x_imag = x_imag + self.pos_embed_imag[:, 1:, :]     # add with positional encoding
         cls_token_imag = self.cls_token_imag + self.pos_embed_imag[:, :1, :]
         cls_tokens_imag = cls_token_imag.expand(x_imag.shape[0], -1, -1)
         x_imag = torch.cat((cls_tokens_imag, x_imag), dim=1)        
@@ -81,7 +81,7 @@ class ChannelBasedTransformer_fft(nn.Module):
         # abs
         x_abs = self.fc_abs(x_abs)
         self.pos_embed_abs = self.fc_position_abs(self.pos_embed) 
-        x_abs = x_abs + self.pos_embed_abs[:, 1:, :]    # 和位置编码相加
+        x_abs = x_abs + self.pos_embed_abs[:, 1:, :]    # add with positional encoding
         cls_token_abs = self.cls_token_abs + self.pos_embed_abs[:, :1, :]
         cls_tokens_abs = cls_token_abs.expand(x_abs.shape[0], -1, -1)
         x_abs = torch.cat((cls_tokens_abs, x_abs), dim=1)        
@@ -93,7 +93,7 @@ class ChannelBasedTransformer_fft(nn.Module):
         x_abs = x_abs.mean(dim=1) if self.pool == 'mean' else x_abs[:, 0]        
         pred_abs = self.classifier_abs(x_abs)       
 
-        # 汇总预测       
+        # weighted sum
         pred = pred_real * self.cls_weight_real +\
                pred_imag * self.cls_weight_imag +\
                pred_abs * self.cls_weight_abs
@@ -113,7 +113,7 @@ if __name__ == '__main__':
     fft_dim = 100
     pool = 'mean'
     model = ChannelBasedTransformer_fft(num_heads, fft_dim, embed_dim, mlp_ratio, dropout, n_chans, num_blocks, num_classes, pool = 'mean')
-    input = torch.rand(batch_size, 113, 100)  # 输入序列
+    input = torch.rand(batch_size, 113, 100)  # input series
     output,_ = model(input,input,input)
     print(output.shape)
     paras = sum([p.data.nelement() for p in model.parameters()])
